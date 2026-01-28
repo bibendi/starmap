c79ecabcfb8f51364ab5d633b277d8662ccade2d
Misha Merkushin <merkushin.m.s@gmail.com>
2026-01-28 23:45:16 +0300

    chore: remove unused tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/frontend/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}',
-    './app/components/**/*.{erb,haml,html,slim,rb}'
-  ],
-}
