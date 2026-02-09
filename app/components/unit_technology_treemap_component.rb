004b1a2143f4f336c9dd6d093e10fe5ec182a88e
Misha Merkushin <merkushin.m.s@gmail.com>
2026-02-10 00:07:29 +0300

    chore: refactor unit_technology_treemap_component

--- app/components/unit_technology_treemap_component.rb
+++ app/components/unit_technology_treemap_component.rb
@@ -8,10 +8,8 @@ class UnitTechnologyTreemapComponent < ViewComponent::Base
   def initialize(teams:)
     @teams = teams
     @technologies_data = calculate
-    # rubocop:disable Rails/Pluck
-    @max_experts = @technologies_data.map { |t| t[:expert_count] }.max || 1
-    @max_deficit = @technologies_data.map { |t| t[:deficit] }.max || 1
-    # rubocop:enable Rails/Pluck
+    @max_experts = max_value(:expert_count)
+    @max_deficit = max_value(:deficit)
   end
 
   def any_technologies?
@@ -55,55 +53,72 @@ class UnitTechnologyTreemapComponent < ViewComponent::Base
     return [] unless current_quarter
 
     team_ids = @teams.map(&:id)
-
-    team_technologies = TeamTechnology
-      .includes(:technology)
-      .where(team_id: team_ids)
-
-    technology_ids = team_technologies.map(&:technology_id).uniq
+    team_technologies = fetch_team_technologies(team_ids)
+    technology_ids = team_technologies.pluck(:technology_id).uniq
 
     return [] if technology_ids.empty?
 
-    expert_ratings = SkillRating
-      .where(
-        quarter: current_quarter,
-        technology_id: technology_ids,
-        rating: EXPERT_MIN_RATING..EXPERT_MAX_RATING,
-        team_id: team_ids
-      )
+    experts_by_tech_team = fetch_experts_by_tech_team(current_quarter, technology_ids, team_ids)
+    build_technologies_data(technology_ids, team_technologies, experts_by_tech_team)
+  end
 
-    experts_by_tech_team = expert_ratings
+  def fetch_team_technologies(team_ids)
+    TeamTechnology.includes(:technology).where(team_id: team_ids)
+  end
+
+  def fetch_experts_by_tech_team(quarter, technology_ids, team_ids)
+    SkillRating
+      .where(quarter: quarter, technology_id: technology_ids, rating: EXPERT_MIN_RATING..EXPERT_MAX_RATING, team_id: team_ids)
       .group(:technology_id, :team_id)
       .count
+  end
 
-    technologies = technology_ids.map do |tech_id|
-      tech_team_records = team_technologies.select { |tt| tt.technology_id == tech_id }
-      next if tech_team_records.empty?
-
-      total_experts = 0
-      total_deficit = 0
-      all_teams_in_target = true
-
-      tech_team_records.each do |team_tech|
-        expert_count = experts_by_tech_team[[tech_id, team_tech.team_id]] || 0
-        total_experts += expert_count
-
-        if expert_count < team_tech.target_experts
-          all_teams_in_target = false
-          total_deficit += (team_tech.target_experts - expert_count)
-        end
-      end
-
-      next if total_experts == 0
-
-      {
-        technology: tech_team_records.first.technology,
-        expert_count: total_experts,
-        all_teams_in_target: all_teams_in_target,
-        deficit: total_deficit
-      }
-    end.compact
+  def build_technologies_data(technology_ids, team_technologies, experts_by_tech_team)
+    technologies = technology_ids.filter_map do |tech_id|
+      calculate_technology_metrics(tech_id, team_technologies, experts_by_tech_team)
+    end
 
     technologies.sort_by { |t| -t[:expert_count] }
+  end
+
+  def calculate_technology_metrics(tech_id, team_technologies, experts_by_tech_team)
+    tech_team_records = team_technologies.select { |tt| tt.technology_id == tech_id }
+    return nil if tech_team_records.empty?
+
+    metrics = aggregate_team_metrics(tech_team_records, tech_id, experts_by_tech_team)
+    return nil if metrics[:total_experts].zero?
+
+    build_technology_hash(tech_team_records.first, metrics)
+  end
+
+  def aggregate_team_metrics(tech_team_records, tech_id, experts_by_tech_team)
+    total_experts = 0
+    total_deficit = 0
+    all_teams_in_target = true
+
+    tech_team_records.each do |team_tech|
+      expert_count = experts_by_tech_team[[tech_id, team_tech.team_id]] || 0
+      total_experts += expert_count
+
+      if expert_count < team_tech.target_experts
+        all_teams_in_target = false
+        total_deficit += (team_tech.target_experts - expert_count)
+      end
+    end
+
+    {total_experts: total_experts, total_deficit: total_deficit, all_teams_in_target: all_teams_in_target}
+  end
+
+  def build_technology_hash(first_record, metrics)
+    {
+      technology: first_record.technology,
+      expert_count: metrics[:total_experts],
+      all_teams_in_target: metrics[:all_teams_in_target],
+      deficit: metrics[:total_deficit]
+    }
+  end
+
+  def max_value(key)
+    [@technologies_data.pluck(key).max, 1].compact.max
   end
 end
